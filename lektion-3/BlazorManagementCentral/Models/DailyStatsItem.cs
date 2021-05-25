1598dcc8f28550dfd7da1bec21c6fb5f1e191336
Hans Mattin-Lassei <hans.mattin-lassei@lexicon.se>
2021-05-25 23:36:11 +0200

    Föreläsning 3

---
+++ lektion-3/BlazorManagementCentral/Models/DailyStatsItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorManagementCentral.Models
+{
+
+    public class DailyStatsItem
+    {
+        public int TotalMessages { get; set; }
+        public int TotalDevices { get; set; }
+        public int TotalNewDevices { get; set; }
+        public int TotalC2DCount { get; set; }
+    }
+
+}
---
+++ lektion-3/BlazorManagementCentral/Models/DeviceItem.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorManagementCentral.Models
+{
+
+    public class DeviceItem
+    {
+        public string DeviceId { get; set; }
+        public DeviceLocation Location { get; set; }
+        public string LastUpdated { get; set; }
+        public DeviceType Type { get; set; }
+        public string Status { get; set; }
+    }
+
+    public class DeviceLocation
+    {
+        public string City { get; set; }
+        public string Country { get; set; }
+    }
+
+    public class DeviceType
+    {
+        public string Sensor { get; set; }
+        public string Description { get; set; }
+    }
+
+}
